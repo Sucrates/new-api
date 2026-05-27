7fafa4dfff135afcc51f8bd77a0efa19cf706bc7
root <root@vmi3096021.contaboserver.net>
2026-05-27 14:56:26 +0800

    Revert "fix: stabilize classic dashboard render"
    This reverts commit de361bb4cbcfd524a90bf894d48129bfaaeab566.

--- web/classic/src/components/common/ErrorBoundary.jsx
+++ web/classic/src/components/common/ErrorBoundary.jsx
@@ -1,22 +1,3 @@
-/*
-Copyright (C) 2025 QuantumNous
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as
-published by the Free Software Foundation, either version 3 of the
-License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU Affero General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License
-along with this program. If not, see <https://www.gnu.org/licenses/>.
-
-For commercial licensing, please contact support@quantumnous.com
-*/
-
 import React from 'react';
 import { Empty, Button } from '@douyinfe/semi-ui';
 import {
@@ -28,14 +9,11 @@ import { withTranslation } from 'react-i18next';
 class ErrorBoundary extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { hasError: false, errorMessage: '' };
+    this.state = { hasError: false };
   }
 
-  static getDerivedStateFromError(error) {
-    return {
-      hasError: true,
-      errorMessage: error?.message || String(error || ''),
-    };
+  static getDerivedStateFromError() {
+    return { hasError: true };
   }
 
   componentDidCatch(error, errorInfo) {
